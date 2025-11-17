8e8651ef92c2b1df786fbf0ad5a7fe635e26b682
Dmitriy A <79046296440@yandex.ru>
2025-11-17 21:14:41 +1000

    1 Commit

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,91 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        // проверяем выполнение необходимых условий
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ConditionsNotMetException("название не может быть пустым");
+        }
+        if (film.getDescription().length() > 200) {
+            throw new ConditionsNotMetException("максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new RuntimeException("дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ConditionsNotMetException("продолжительность фильма должна быть положительным числом");
+        }
+        // формируем дополнительные данные
+        film.setId(getNextId());
+
+        // сохраняем новую публикацию в памяти приложения
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    // вспомогательный метод для генерации идентификатора нового поста
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (!(newFilm.getName() == null || newFilm.getName().isBlank())) {
+                oldFilm.setName(newFilm.getName());
+            }
+            if (newFilm.getDescription().length() <= 200) {
+                oldFilm.setDescription(newFilm.getDescription());
+
+            }
+            if (newFilm.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28))) {
+                oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            }
+
+            if (newFilm.getDuration() > 0) {
+                oldFilm.setDuration(newFilm.getDuration());
+
+
+            }
+            return oldFilm;
+        } else {
+            throw new NotFoundException("Film с id = " + newFilm.getId() + " не найден");
+        }
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,88 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ConditionsNotMetException("Имейл должен быть указан и содержать символ @");
+        }
+        if (users.values().stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ConditionsNotMetException("Логин не может быть пустым или содержать пробелы");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getEmail());
+        }
+        if (user.getBirthday().isAfter(Instant.now())) {
+            throw new RuntimeException("дата рождения не может быть в будущем");
+        }
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet().stream().mapToLong(id -> id).max().orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.values().stream().anyMatch(u -> u.getEmail().equals(newUser.getEmail()) && !u.getId().equals(newUser.getId()))) {
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            if (!(newUser.getEmail() == null || newUser.getEmail().isBlank() || !newUser.getEmail().contains("@"))) {
+                oldUser.setEmail(newUser.getEmail());
+
+            }
+            if (!(newUser.getLogin() == null || newUser.getLogin().isBlank() || newUser.getLogin().contains(" "))) {
+                oldUser.setLogin(newUser.getLogin());
+
+            }
+            if (!(newUser.getName() == null || newUser.getName().isBlank())) {
+                oldUser.setName(newUser.getName());
+            }
+
+            if (newUser.getBirthday().isBefore(Instant.now())) {
+                oldUser.setBirthday(newUser.getBirthday());
+
+
+            }
+            return oldUser;
+        } else {
+            throw new NotFoundException("Пост с id = " + newUser.getId() + " не найден");
+        }
+    }
+}
+
